d5be3e22dcf7ade88dda852ec357d5bbf63a89e0
Vaishnavi Phadake <vaishnaviphadake@gmail.com>
2025-07-07 15:34:21 +0530

    fixed navbar

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -28,7 +28,7 @@ export const Header = () => {
 
 	return (
 		<div className="flex justify-center items-center fixed top-6 inset-x-0 z-[9999]">
-			<nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+			<nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur mb-3">
 				{navItems.map((item) => (
 					<a
 						key={item.id}
